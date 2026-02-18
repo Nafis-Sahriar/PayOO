e70dda8aceff3b78572e14b43055644b5ccccd6d
Nafis-Sahriar <nafis.apid@gmail.com>
2026-02-18 13:27:14 +0600

    Machiene -1 Added and Cashout Code Optimized with Machiene

--- JS/cashout.js
+++ JS/cashout.js
@@ -1,82 +1,132 @@
 document.getElementById('cashout-btn').addEventListener('click', function()
 {
+    //_____________________________________________With Machiene________________________________________________
 
-    // console.log("Cashout Button Clicked"); //--> ok
+    //1------> Get the Agent Number and Validate
+
+    const cashoutNumber = getValueFromInput('cashout-number');
+    // console.log(cashoutNumber); // ekhane value ashbe na cz return korini
+
+    if(cashoutNumber.length!=11)
+    {
+        alert("Invalid Number");
+        return;
+    }
+
+
+    //2------> Get the CashoutAmount
+    const cashoutAmount = getValueFromInput('cashout-amount');
+    // console.log(cashoutAmount);
+
+    //3------> Get the Balance
+
+    const balanceElement = document.getElementById('balance');
+    const balance = balanceElement.innerText;
+    console.log(balance);
+
+    // 4-----> Calculate Balance
+
+    const newBalance = Number(balance) - Number(cashoutAmount);
+    console.log(newBalance);
+    if(newBalance<0)
+    {
+        console.log("Invalid Amount");
+        alert("Insufficient Balance")
+        return;
+    }
+
+    const pin = getValueFromInput('cashout-pin');
+    if(pin==='1234')
+    {
+        alert("Cashout Successful");
+        balanceElement.innerText=newBalance;
+    }
+    else
+    {
+        alert("Invalid Pin Code");
+        return;
+    }
+
+  //________________________________________________Without Machiene________________________________________________
+
+
+
+    // // console.log("Cashout Button Clicked"); //--> ok
 
   
-    // 1. Get the agent Number and Validate 
+    // // 1. Get the agent Number and Validate 
 
-        const agentNumber = document.getElementById('cashout-number');
-        const number = agentNumber.value;
+    //     const agentNumber = document.getElementById('cashout-number');
+    //     const number = agentNumber.value;
 
-        console.log(number); 
+    //     console.log(number); 
 
-        //validation_Number
-        if(number.length !=11)
-        {
-            alert("Invalid Agent Number");
-            return;
-        }
+    //     //validation_Number
+    //     if(number.length !=11)
+    //     {
+    //         alert("Invalid Agent Number");
+    //         return;
+    //     }
 
-        // 2. Get the amount, validate, convert to Number
-        const cashoutAmountInput = document.getElementById('cashout-amount');
-        const cashoutAmount=cashoutAmountInput.value;
-        console.log(cashoutAmount);
+    //     // 2. Get the amount, validate, convert to Number
+    //     const cashoutAmountInput = document.getElementById('cashout-amount');
+    //     const cashoutAmount=cashoutAmountInput.value;
+    //     console.log(cashoutAmount);
 
-        // 3. Get the current balance, validate, convert to number 
+    //     // 3. Get the current balance, validate, convert to number 
 
-        const balanceElement = document.getElementById('balance');
-        console.log(balanceElement);
-
-
-
-        // This is My Own Way {
-        // const balanceValue = parseInt(balanceElement.innerText);
-        // console.log(balanceValue);
-
-        // const newBalance = balanceValue-parseInt(cashoutAmount);
-
-        // console.log(newBalance);}
-
-        // Instructor's Way --> 
-
-         const balance = balanceElement.innerText;
-
-         const newBalance = Number(balance-cashoutAmount);
-         console.log(newBalance);
-
-         if(newBalance<=0)
-         {
-            alert("Invalid Amount");
-            return;
-         }
+    //     const balanceElement = document.getElementById('balance');
+    //     console.log(balanceElement);
 
 
 
-         // Pin validation
+    //     // This is My Own Way {
+    //     // const balanceValue = parseInt(balanceElement.innerText);
+    //     // console.log(balanceValue);
 
-         const cashoutPinInput = document.getElementById('cashout-pin');
-         const pin = cashoutPinInput.value;
-         console.log(pin);
+    //     // const newBalance = balanceValue-parseInt(cashoutAmount);
 
-         if(pin==='1234')
-         {
-            alert("Cashout Successful");
-            // console.log(newBalance);
-            balanceElement.innerText=newBalance;
+    //     // console.log(newBalance);}
 
-         }
-         else
-         {
-            alert("Invalid Pin");
-            return;
-         }
+    //     // Instructor's Way --> 
 
-    // 3. Get the current balance, validate, convert to number 
-    // 4. Calculate the new balance 
-    // 5. Get the pin , and verify 
-    //    >> true:: show an alert --> Set balance
-    //    >> false:: show an alert --> Error--> Return
+    //      const balance = balanceElement.innerText;
+
+    //      const newBalance = Number(balance-cashoutAmount);
+    //      console.log(newBalance);
+
+    //      if(newBalance<=0)
+    //      {
+    //         alert("Invalid Amount");
+    //         return;
+    //      }
+
+
+
+    //      // Pin validation
+
+    //      const cashoutPinInput = document.getElementById('cashout-pin');
+    //      const pin = cashoutPinInput.value;
+    //      console.log(pin);
+
+    //      if(pin==='1234')
+    //      {
+    //         alert("Cashout Successful");
+    //         // console.log(newBalance);
+    //         balanceElement.innerText=newBalance;
+
+    //      }
+    //      else
+    //      {
+    //         alert("Invalid Pin");
+    //         return;
+    //      }
+
+    // // 3. Get the current balance, validate, convert to number 
+    // // 4. Calculate the new balance 
+    // // 5. Get the pin , and verify 
+    // //    >> true:: show an alert --> Set balance
+    // //    >> false:: show an alert --> Error--> Return
 
 
     
--- JS/machiene.js
+++ JS/machiene.js
@@ -1,3 +1,11 @@
 console.log("Machiene Added");
 
 //ekhane o jekuno ekta element er id niye input value ta amake dibe. 
+
+function getValueFromInput(id){
+    const input = document.getElementById(id);
+    const value = input.value;
+    console.log(id, value);
+    return value;
+
+}
